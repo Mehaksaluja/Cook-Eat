862e9796680807d9a6baf39ffe7daf20e48d81e6
Mehaksaluja <salujamehak11@gmail.com>
2025-07-21 11:49:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipe Rover',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF6B6B)),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF7F7F7),
+        fontFamily: 'Poppins',
+      ),
+      home: const Text("App is Running"),
+    );
+  }
+}
